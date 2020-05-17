08d4ab28a8f67f8a4d41512da51f4db2182923f5
jermicide <jeremy@lakey.net>
2020-05-17 17:02:12 -0500

    may the 0th

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 function updateTime() {
-    $('.date-time').text(moment().format("ddd, MMM do, h:mm:ss a"));
+    $('.date-time').text(moment().format("ddd, MMM Do, h:mm:ss a"));
     setTimeout(updateTime, 1000);
 }
-(function($) {
 
+(function($) {
+    var format = 'hh:mm:ss'
     updateTime();
+
 
     $.get("https://hotchicks.lakey.net/temp.json", function(data) {
         $('#pitemp').text(data.temp);
